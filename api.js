795e8368d8b015fc0fc3b819db61e73a3a44e002
agorman <gorman.colorado@gmail.com>
2024-04-15 23:06:44 -0600

    Changed Express port to 5010 because it 5000 is used by Mac Airplay

--- api.js
+++ api.js
@@ -143,6 +143,6 @@ app.use((err, req, res, next) => {
   res.status(500).send('Server error occurred');
 });
 
-app.listen(5000, () => {
-  console.log('Backend started at: http://localhost:5000');
+app.listen(5010, () => {
+  console.log('Backend started at: http://localhost:5010');
 });
